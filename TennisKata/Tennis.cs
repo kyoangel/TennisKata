5b8a12c89378754e5039574d0e0dce72bfe007fe
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2019-06-10 14:46:54 +0800

    Thirty All

--- TennisKata/Tennis.cs
+++ TennisKata/Tennis.cs
@@ -2,9 +2,25 @@
 {
 	public class Tennis
 	{
+		private int _firstPlayerScoreTimes;
+
 		public string Score()
 		{
+			if (_firstPlayerScoreTimes == 2)
+			{
+				return "Thirty Love";
+			}
+			if (_firstPlayerScoreTimes == 1)
+			{
+				return "Fifteen Love";
+			}
+
 			return "Love All";
+		}
+
+		public void FirstPlayerScore()
+		{
+			_firstPlayerScoreTimes++;
 		}
 	}
 }
--- TennisKata/UnitTest1.cs
+++ TennisKata/UnitTest1.cs
@@ -13,6 +13,21 @@ namespace TennisKata
 			ScoreShouldBe("Love All");
 		}
 
+		[TestMethod]
+		public void Fifteen_Love()
+		{
+			_tennis.FirstPlayerScore();
+			ScoreShouldBe("Fifteen Love");
+		}
+
+		[TestMethod]
+		public void Thirty_Love()
+		{
+			_tennis.FirstPlayerScore();
+			_tennis.FirstPlayerScore();
+			ScoreShouldBe("Thirty Love");
+		}
+
 		private void ScoreShouldBe(string expected)
 		{
 			var score = _tennis.Score();
