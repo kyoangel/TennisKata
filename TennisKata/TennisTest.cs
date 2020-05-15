f4d4b8e71811a52775a2346d8ddcb7f676fb4139
kyo.lai82 <kyo.lai82@titansoft.com.sg>
2020-05-15 08:07:21 +0800

    rename test

---
+++ TennisKata/TennisTest.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisKata
+{
+    [TestClass]
+    public class TennisTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
