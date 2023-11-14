219d8149ca4ab7939de6a4a4b40366cf3542b6ba
jerry.chen38 <jerry.chen@titansoft.com.sg>
2023-11-14 18:53:08 +0800

    add Fifteen_Love test

--- TennisKata/TennisGame.cs
+++ TennisKata/TennisGame.cs
@@ -2,8 +2,20 @@
 
 public class TennisGame
 {
+    private int _firstPlayerScoreTime;
+
     public string Score()
     {
+        if (_firstPlayerScoreTime==1)
+        {
+            return "Fifteen Love";
+        }
+
         return "Love All";
+    }
+
+    public void FirstPlayerScore()
+    {
+        _firstPlayerScoreTime++;
     }
 }
--- TennisKata/TennisGameTests.cs
+++ TennisKata/TennisGameTests.cs
@@ -4,16 +4,26 @@ namespace TennisKata;
 
 public class Tests
 {
+    private TennisGame _tennisGame;
+
     [SetUp]
     public void Setup()
     {
+        _tennisGame = new TennisGame();
     }
 
     [Test]
     public void Love_All()
     {
-        var tennisGame = new TennisGame();
-        var score = tennisGame.Score();
+        var score = _tennisGame.Score();
         score.Should().Be("Love All");
+    }
+
+    [Test]
+    public void Fifteen_Love()
+    {
+        _tennisGame.FirstPlayerScore();
+        var score = _tennisGame.Score();
+        score.Should().Be("Fifteen Love");
     }
 }
